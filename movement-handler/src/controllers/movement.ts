310f43b354bb9c48f2382471a179ee2748984ce7
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2019-01-27 21:21:37 +0200

    Refactor code

--- movement-handler/src/controllers/movement.ts
+++ movement-handler/src/controllers/movement.ts
@@ -1,3 +1,18 @@
-export const create = (movement: JSON) => {
-    console.log(movement);
+export type Movement = {
+    playerID: Number;
+    cordX: Number;
+    cordZ: Number;
+    rotation: Number;
+    inputX: Number;
+    inputZ: Number;
 };
+
+const movementController = (publisher: (msg: string) => void) => {
+    return {
+        create: (movement: Movement) => {
+            publisher('asd');
+        }
+    }
+}
+
+export default movementController;
--- movement-handler/src/index.ts
+++ movement-handler/src/index.ts
@@ -1,18 +1,13 @@
-import * as amqplib from 'amqplib';
-import * as movementController from './controllers/movement';
+import messageBroker from './rabbitmq/client';
+import movementController, { Movement } from './controllers/movement';
 
-const que = 'movement';
+messageBroker('amqp://rabbitmq', 'movement', 'outgoing').then(mb =>
+    mb.publisher.then(publisher => {
+        const movement = movementController(publisher);
 
-amqplib
-    .connect('amqp://rabbitmq')
-    .then(conn => conn.createChannel())
-    .then(ch =>
-        ch.assertQueue(que, { durable: false }).then(ok =>
-            ch.consume(que, msg => {
-                if (msg === null) return;
-                ch.ack(msg);
-                movementController.create(JSON.parse(msg.content.toString()));
-            })
-        )
-    )
-    .catch(console.warn);
+        mb.subscribe((msg: Movement) => {
+            console.log(msg);
+            movement.create(msg);
+        });
+    })
+);
