b091a3cb493a16de3eecbbb6fac93e066f16fe23
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2019-02-15 17:26:14 +0200

    Code cleaning

--- movement-handler/src/movement/index.ts
+++ movement-handler/src/movement/index.ts
@@ -5,16 +5,17 @@ const client = redis.createClient({ host: 'redis' });
 
 const move = (publish: Function) => (movement: Movement) => {
     const { playerId, ...rest } = movement;
+    // Save movement in state
     client.set(`player:${playerId}:movement`, JSON.stringify(rest));
-    client.get('game:1:players', (_err, players) => {
-        JSON.parse(players).forEach((player: Player) => {
-            if (player.id === playerId) return;
-            publish(JSON.stringify({ to: player.address, message: movement }));
-        });
-    });
+    // Send movement to other players
+    client.get('game:1:players', (_err, players) =>
+        JSON.parse(players).forEach((player: Player) =>
+            player.id !== playerId ? publish(JSON.stringify({ to: player.address, message: movement })) : null
+        )
+    );
 };
 
-const movementHandler = (publish: (msg: string) => boolean) => ({
+const movementHandler = (publish: Function) => ({
     move: move(publish)
 });
 
