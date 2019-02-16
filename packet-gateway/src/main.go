6fa8099893e51ee4a515fdc698a4289c1cd1fa91
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2019-02-16 16:33:59 +0200

    Add support for outward messages

--- packet-gateway/src/main.go
+++ packet-gateway/src/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"log"
 	"udp"
+	"conn"
+	"err"
+	"net"
+	"rabbitmq"
 )
 
 func main() {
 	log.Println("Starting packet gateway...")
+	initializeUdpConnector()
+	defer conn.Udp.Close()
 	go udp.Listener()
+	go rabbitmq.Listener("outward")
 	select {}
+}
+
+func initializeUdpConnector() {
+	var e error
+	conn.Udp, e = net.ListenPacket("udp", ":3000")
+	err.Handler(e, "Can't create udp connector")
 }
