f18fc21a0624c7f9795a7a150b278b6f54acfefb
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2019-02-23 14:02:20 +0200

    Add redis abstraction layer

--- movement-handler/src/movement/index.ts
+++ movement-handler/src/movement/index.ts
@@ -1,5 +1,5 @@
 import { Movement, Player } from '../types';
-import * as redis from 'redis';
+import * as redis from '../redis';
 
 const client = redis.createClient({ host: 'redis' });
 
@@ -7,15 +7,13 @@ const move = (publish: Function) => (movement: Movement) => {
     console.log('Received movement:', movement);
     const { playerId, ...rest } = movement;
     // Save movement in state
-    client.set(`player:${playerId}:movement`, JSON.stringify(rest));
+    client.set(`player:${playerId}:movement`, rest);
     // Send movement to other players
-    client.get('game:1:players', (_err, players) =>
+    client.get('game:1:players', (err: Error | null, players: Player[]) =>
         publish(
             JSON.stringify({
                 type: 3,
-                to: JSON.parse(players)
-                    .filter((p: Player) => p.id !== playerId)
-                    .map((p: Player) => p.address),
+                to: players.filter((p: Player) => p.id !== playerId).map((p: Player) => p.address),
                 content: movement
             })
         )
